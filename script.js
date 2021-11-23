53838aa2d8d44f2b2058174c49e255814f586bed
nehCnitsuJ <s10222137@connect.np.edu.sg>
2021-11-23 22:03:24 +0800

    js formatting

--- script.js
+++ script.js
@@ -1,24 +1,11 @@
-var mybutton = document.getElementById("topbutton");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
-function displaymessage()
+/*function displaymessage()
 {
   document.write("Thank you! \n")
   document.write("Refresh the page to go back.")
 }
-
+*/

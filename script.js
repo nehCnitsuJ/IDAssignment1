964f7ad5da40c2abaaf65092d1a0814847ca7d1e
nehCnitsuJ <s10222137@connect.np.edu.sg>
2021-11-27 20:16:25 +0800

    scroll to top bottom feature

---
+++ script.js
@@ -0,0 +1,8 @@
+//Get button:
+mybutton = document.getElementById("myBtn");
+
+// scroll to top function
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}

0ad3c317982de66e0fb9ca45d48943a7a480e818
nehCnitsuJ <s10222137@connect.np.edu.sg>
2021-11-27 19:07:50 +0800

    scroll to top button

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
-// When the user clicks on the button, scroll to the top of the document
+//Get button:
+mybutton = document.getElementById("scrolltotop");
+
+//when scroll down 30px, button is shown immediately
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// scroll to top functioon
 function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
+  document.documentElement.scrollTop = 0; 
 }
-/*function displaymessage()
-{
-  document.write("Thank you! \n")
-  document.write("Refresh the page to go back.")
-}
-*/

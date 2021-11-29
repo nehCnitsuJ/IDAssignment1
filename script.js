84c4b8f505331bf7dd5247881b4e4c3bf7b393f0
nehCnitsuJ <s10222137@connect.np.edu.sg>
2021-11-29 10:43:12 +0800

    js function for nav bar

--- script.js
+++ script.js
@@ -6,3 +6,12 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}

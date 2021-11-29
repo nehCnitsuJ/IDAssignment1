2037677f844ec2662e872fdf63be2d8afeac83a1
nehCnitsuJ <s10222137@connect.np.edu.sg>
2021-11-29 17:10:16 +0800

    popup display message

--- script.js
+++ script.js
@@ -21,4 +21,6 @@ function myFunction() {
 function toggle(){
   var blur = document.getElementById('blur');
   blur.classList.toggle('active')
+  var popup = document.getElementById('popup');
+  popup.classList.toggle('active')
 }
